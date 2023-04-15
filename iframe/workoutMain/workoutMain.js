3cf90af5d2ce6c6d1d94625d6eb117cd10778ae5
Martim Pereira <fc58223@alunos.fc.ul.pt>
2023-04-15 01:10:56 +0100

    teste com events

--- iframe/workoutMain/workoutMain.js
+++ iframe/workoutMain/workoutMain.js
@@ -5,49 +5,8 @@ back.addEventListener("click", () => {
 });
 
 // trying swipe //
-const s = document.getElementById("screen");
-s.addEventListener('touchstart', handleTouchStart, false);        
-s.addEventListener('touchmove', handleTouchMove, false);
 
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-                                                                         
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-                                                                         
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            document.location.href = "../mainMenu/mainMenu.html"
-        } else {
-            /* left swipe */
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */ 
-        } else { 
-            /* up swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+document.addEventListener("swiped-right", function() {
+    prompt("yooooo");
+    document.location.href = "../mainMenu/mainMenu.html";
+});
